f5b785f95dfcdad31ef773987bb44545679dadd6
RuiOliveira83 <rui.p3ld@gmail.com>
2021-09-28 20:19:53 +0000

    Added Products app and models

--- code_aquatics/settings.py
+++ code_aquatics/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # requested by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
